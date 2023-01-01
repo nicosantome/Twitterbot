9e2d425e4fd761d2207b41e3b1787e44f0056e5d
nicosantome <nicosantome@gmail.com>
2023-01-01 23:04:18 +0100

    Ready to first deploy.

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
 import fetch from "node-fetch";
 import Datastore from "nedb";
+import Twit from "twit";
 
+//This first DB saves all products from the web store
 const db = new Datastore("database.db");
-const dailyUpdate = new Datastore("dailyUpdate.db");
-
 db.loadDatabase();
+
+//This second DB saves only the products which price has just been updated
+const dailyUpdate = new Datastore("dailyUpdate.db");
 dailyUpdate.loadDatabase();
 
-let todaysIds = [];
-
-// Getting all categories from Mercadona
-async function getCategories() {
-  const categoriesArr = [];
-  const url = "https://tienda.mercadona.es/api/categories/";
-  const res = await fetch(url);
-  const data = await res.json();
-  const supraCat = data.results;
-  supraCat.forEach((cat) => {
-    const categories = cat.categories;
-    categories.forEach((cat) => categoriesArr.push(cat.id));
-  });
-  return categoriesArr;
-}
-
-const categoriesId = await getCategories();
-
-// Getting product names & prices
-categoriesId.forEach((id, i) => {
-  setTimeout(() => {
-    getProducts(id);
-    console.log(i);
-  }, i * 3000);
+// Twitter library
+const T = new Twit({
+  consumer_key: "zBvUi9GyGh6Ggp1QYYnTifrs4",
+  consumer_secret: "EMFrJMWilJZ4fxrG7yKjgUySIEtfrgXaWtqTaJiTPBd2SIztQH",
+  access_token: "1600579008839696417-izfg1tLMgopTKThbJf92zSUYywbvhQ",
+  access_token_secret: "aapLmnDezZEvrIOF3TZHLBkC450NszgK4N3NoZtMy4pqu",
 });
 
+//This next 3 secondary functions are for the main getProducts function work later
 //Function checks if a product already exists in the database. If it does, it returns the product and if not it returns false.
 function idExists(id) {
   return new Promise((resolve, reject) => {
@@ -74,7 +60,25 @@ function createNewProduct(product) {
   db.insert(prod);
 }
 
-//Function fetches all products from a specific category (example used when calling the fn: 115)
+//********* Product fetch + DBs maintainance *********
+
+// Getting all product categories from Mercadona
+async function getCategories() {
+  const categoriesArr = [];
+  const url = "https://tienda.mercadona.es/api/categories/";
+  const res = await fetch(url);
+  const data = await res.json();
+  const supraCat = data.results;
+  supraCat.forEach((cat) => {
+    const categories = cat.categories;
+    categories.forEach((cat) => categoriesArr.push(cat.id));
+  });
+  return categoriesArr;
+}
+
+const categoriesId = await getCategories();
+
+//Function fetches all products from a specific category (catId) of the supermarket, it creates and updates "db" database with all products and it creates a "dailyUpdate" db with the price variation products that will be tweeted later on.
 async function getProducts(catId) {
   const url = `https://tienda.mercadona.es/api/categories/${catId}`;
   const res = await fetch(url);
@@ -96,8 +100,68 @@ async function getProducts(catId) {
           let newPriceId = product.id;
           db.update({ _id: product.id }, { $push: { prices: newPrice } });
           dailyUpdate.insert({ newPriceId });
+          db.persistence.compactDatafile();
         }
       }
     });
   });
 }
+
+// ********** TwitterBot functionality **********
+
+// Function creates a string with the price increase or decrease of each product on dailyUpdate DB and posts the string to tweeter
+function createPost(id) {
+  //Compares last price and previous price and inform if "Increase" or "Decrease", how much and the percentage of the change
+  db.find({ _id: id }, function (err, docs) {
+    let lastPrice = docs[0].prices[docs[0].prices.length - 1].price;
+    let prevPrice = docs[0].prices[docs[0].prices.length - 2].price;
+    let update = lastPrice > prevPrice ? "Aumento" : "Bajada";
+
+    let diferencia =
+      update == "Aumento"
+        ? (lastPrice - prevPrice) / prevPrice
+        : (prevPrice - lastPrice) / prevPrice;
+    let str = `
+      ${docs[0].name}\n
+      ${update} de precio\n
+      Antes: €${docs[0].prices[docs[0].prices.length - 2].price}\n
+      Ahora: €${docs[0].prices[docs[0].prices.length - 1].price}\n
+      ${update == "Aumento" ? "🔺" : "🔻"} ${(diferencia * 100).toFixed()}%
+      `;
+
+    //This below post the string created
+    T.post("statuses/update", { status: str }, function (err, data, response) {
+      if (err) console.error(err);
+      else console.log(response.statusCode, response.statusMessage);
+    });
+  });
+}
+
+function product() {
+  return new Promise((resolve, reject) => {
+    categoriesId.forEach((id, i) => {
+      setTimeout(() => {
+        getProducts(id);
+      }, i * 4000);
+    });
+    setTimeout(resolve, categoriesId.length * 4000);
+  });
+}
+
+function tweet() {
+  return new Promise((resolve, reject) => {
+    dailyUpdate.find({}, function (err, docs) {
+      docs.forEach((doc, i) => {
+        setTimeout(() => {
+          // The timeout is as per Twitter permited posts per minute
+          createPost(doc.newPriceId);
+        }, i * 10000);
+      });
+      dailyUpdate.remove({}, { multi: true });
+      dailyUpdate.persistence.compactDatafile();
+      resolve();
+    });
+  });
+}
+
+product().then(tweet);
